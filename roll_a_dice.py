6e18eb026049e9e065fec01ec8ac9be904a3fd5d
gnzat <tazngmt@gmail.com>
2022-03-07 15:01:00 +0800

    roll_a_dice

---
+++ roll_a_dice.py
@@ -0,0 +1,20 @@
+#import library for random number generator
+import random
+#range of values on dice
+minval=1
+maxval=6
+#to loop roling through user input
+roll_again = "yes"
+#loop
+while roll_again == "yes" or roll_again == "y":
+    print("Rolling The Dices...")
+    print("The Values are :")
+    
+    #generating and printing 1st random integer from 1 to 6
+    print(random.randint(min_val, max_val))
+    
+    #generating and printing 2nd random integer from 1 to 6
+    print(random.randint(min_val, max_val))
+    
+    #asking user to roll the dice again. Any input other than yes or y will terminate the loop
+    roll_again = input("Roll the Dices Again?") 
